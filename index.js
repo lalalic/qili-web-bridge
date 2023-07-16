b3ae3f2acfe535f13ed2f563565077909278cb8c
lalalic <lalalic@139.com>
2023-07-16 15:35:19 -0400

    uncomment

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 Cloud.addModule({
     ...require("react-native-use-qili/cloud/web-proxy")(
-        // (()=>{
-        //     let i=0
-        //     const pubsub=new (require("graphql-redis-subscriptions").RedisPubSub)({
-        //         connection: {
-        //             host:"qili.pubsub",
-        //         },
-        //         connectionListener(err){
-        //             if(err?.errno==-3008){
-        //                 i++
-        //                 if(i==3){
-        //                     pubsub.closed=true
-        //                     console.error(`redis can't be connected, tried 3 times, but still failed, quit.`)
-        //                     pubsub.close()
-        //                 }
-        //             }
-        //         } 
-        //     })
-        //     return pubsub
-        // })()
+        (()=>{
+            let i=0
+            const pubsub=new (require("graphql-redis-subscriptions").RedisPubSub)({
+                connection: {
+                    host:"qili.pubsub",
+                },
+                connectionListener(err){
+                    if(err?.errno==-3008){
+                        i++
+                        if(i==3){
+                            pubsub.closed=true
+                            console.error(`redis can't be connected, tried 3 times, but still failed, quit.`)
+                            pubsub.close()
+                        }
+                    }
+                } 
+            })
+            return pubsub
+        })()
     ),
 })
 
