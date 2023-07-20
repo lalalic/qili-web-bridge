4decc0c262108171eafd88c49a11b73fa615adf7
lalalic <lalalic@139.com>
2023-07-20 11:29:03 -0400

    latest use-qili

--- index.js
+++ index.js
@@ -15,24 +15,4 @@ Cloud.addModule({
 
 Cloud.addModule(Cloud.AccessToken)
 
-__DEV__ && Cloud.addModule({
-    name:"events",
-    events:{
-        graphql(request){
-            console.debug(`graphql: ${request?.query.replace(/\s+/g," ")}`)
-        },
-        load(){
-            console.info('is ready')
-        },
-        ["static"](url){
-            console.debug(`> ${url}`)
-        },
-
-        ["static.matched"](url){
-            console.debug(`>> ${url}`)
-        },
-        ["static.no"](url){
-            console.debug(`>> !${url}`)
-        }
-    }
-})
+Cloud.addModule(require("react-native-use-qili/cloud/events"))
